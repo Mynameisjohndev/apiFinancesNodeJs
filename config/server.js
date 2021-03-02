ef818fae7edfbc50bbf1b181da443388938caa07
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-02 14:49:00 -0300

    Inserting cors into the api

--- config/server.js
+++ config/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const server = express()
+const cors = require('cors');
 
 //require('../api/billingCycle/billingCycle');
 //const BS = mongoose.model('billingCycle');
 
+server.use((req, res, next)=>{
+   res.header("Access-Control-Allow-Origin", "*")
+   server.use(cors());
+   next();
+})
 server.use(bodyparser.urlencoded({ extended: false }));
 server.use(bodyparser.json());
 server.use(bodyparser.raw());
