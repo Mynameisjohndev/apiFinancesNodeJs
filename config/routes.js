c2152dbaef9cbf7ebe98afd97cb8490bdc536898
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-25 15:11:11 -0300

    Updating routes with authentication

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,52 @@
-const express = require('express');
-
-module.exports = function(server) {
-
-    const router = express.Router();
-    server.use("/api", router);
-
-    const billingCycleServices = require("../api/billingCycle/billingCycleServices");
-    billingCycleServices.register(router, "/billingCycles");
-
-    const billingCyclesSummary = require("../api/billingSummary/billingSummaryServices");
-    router.route("/billingSummary").get(billingCyclesSummary.getSummary)
+const express = require('express')
+const auth = require('./auth')
+module.exports = function (server) {
+  /*
+  * Rotas abertas
+  */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+  const AuthService = require('../api/user/userAuth')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
+  /*
+  * Rotas protegidas por Token JWT
+  */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  protectedApi.use(auth)
+  const billingCycleService = require(
+    '../api/billingCycle/billingCycleServices')
+  billingCycleService.register(protectedApi, '/billingCycles')
+  const billingSummaryService =
+    require('../api/billingSummary/billingSummaryServices')
+  protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 }
+
+
+
+//SEM AUTENTICAÇÂO
+
+// const express = require('express')
+
+// module.exports = function(server){
+
+//   //API routes
+//   const router = express.Router()
+//   server.use('/api', router)
+
+//   // rotas da api
+//   const billingCycleService = require('../api/billingCycle/billingCycleService')
+//   billingCycleService.register(router, '/billingCycles')
+
+// const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+// router.route('/billingSummary').get(billingSummaryService.getSummary)
+
+
+
+//   //router.route('/teste').get(function(req, res, next){
+//     //res.send('Funcionou')
+//   //})
+
+// }
